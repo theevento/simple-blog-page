e37503a1c28e3b7776a7f092fdcf2049fd1a216b
theevento <kontakt@pawelpieczonka.pl>
2018-11-07 21:02:11 +0100

    Update readme and fix other bugs

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # Simple blog page
 
 ## Build application
-Build via docker:
+Clone repository:
+```bash
+$ git clone https://github.com/theevento/simple-blog-page
+```
+Go to repository directory:
+```bash
+$ cd simple-blog-page
+```
+Set permission to data directory:
+
+```bash
+$ sudo chmod -R 777 data/
+```
+Build image via docker:
 ```bash
 $ docker-compose up -d --build
 ```
+Main page:
+```text
+http://127.0.0.33/blog
+```
+Login page:
+```text
+http://127.0.0.33/login
+```
+Default data for login:
+```text
+Username: admin
+Password: 123
+```
+And that's it!
---
+++ schema/schema.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: blog_database
+-- Generation Time: Nov 07, 2018 at 07:03 PM
+-- Server version: 5.6.42
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `created_date` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `title`, `description`, `content`, `created_date`, `active`) VALUES
+(1, 1, 'Pierwszy post na stronie', 'Oto TwÃ³j pierwszy post na stronie, moÅ¼esz go edytowaÄ‡ w panelu administratora', 'Witaj,\r\nOto TwÃ³j pierwszy post pt. \"{POST_TITLE}\", moÅ¼esz edytowaÄ‡ jego treÅ›Ä‡ w panelu administracyjnym!', 1541617170, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `name`, `password`, `active`) VALUES
+(1, 'admin', 'Jan Kowalski', '$2a$04$zTzS7E1YvcCrB7c9Z.nkZeJxQK1DPV1I8NSterv0YKDY0ScX02Hnu', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_885DBAFAA76ED395` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `FK_885DBAFAA76ED395` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
